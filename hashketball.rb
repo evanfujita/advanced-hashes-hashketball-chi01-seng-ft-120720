38b6b6ec465a987044907a466623b1d8c8ed7c2e
Evan Fujita <evanfujita@gmail.com>
2020-11-15 04:52:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ def player_with_longest_name
   count = 0
   length = 0
   game_hash[:home].each do |key, value|
-   name_array.push(key[:player_name])
+   name_array.push(key[:players][count][:player_name])
    count += 1
     end
   name_array
