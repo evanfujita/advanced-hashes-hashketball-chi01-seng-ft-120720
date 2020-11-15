30a596b5281aad7ba27f7ca07c42e0a18c5845f5
Evan Fujita <evanfujita@gmail.com>
2020-11-15 05:12:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,7 +196,7 @@ def long_name_steals_a_ton?
       name = key[:player_name]
       steal_hash[name] = key[:steals]
     end
-    steal_hash
+    steal_hash.max
     
 end
     long_name_steals_a_ton?
